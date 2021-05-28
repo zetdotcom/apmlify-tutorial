dd8faa3613742712397203c7590361b5ec1e4c69
Michal Zet <michal.zadarnowski@gmail.com>
2021-05-28 23:11:16 +0100

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Hello from V2
+          Hello from V2s
         </a>
       </header>
       <AmplifySignOut />
